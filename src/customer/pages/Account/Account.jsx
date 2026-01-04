218c0141d61bfc1e537a57e3bfe87284d7afbaae
nrana <rananitish37@gmail.com>
2026-01-04 23:14:50 +0530

    Created account profile(all order items)

--- src/customer/pages/Account/Account.jsx
+++ src/customer/pages/Account/Account.jsx
@@ -1,25 +1,49 @@
-import { Divider } from '@mui/material'
-import React from 'react'
+import { Divider } from "@mui/material";
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Orders from "./Orders";
+
+const menu = [
+  { name: "orders", path: "/account/orders" },
+  { name: "profile", path: "/account/profiles" },
+  { name: "Save Cards", path: "/account/saved-card" },
+  { name: "Address", path: "/account/addresses" },
+  { name: "Logout", path: "/" },
+];
 
 const Account = () => {
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const handleClick = (item) => navigate(item.path);
+
   return (
     // <div className='px-5 lg:px-52 min-h-screen mt-10'>
-        <div className='lg:px-52 min-h-screen mt-10'>
-        <div>
-            <h1 className='text-xl font-bold pb-5'>Codex</h1>
-        </div>
-        <Divider/>
-        <div className='grid grid-cols-1 lg:grid-cols-3 lg:min-h-[78vh]'>
-            <section className='col-span-1 lg:border-r lg:pr-5 py-5'>
-                Left
-            </section>
-            <section>
-                Right
-            </section>
-        </div>
-      
+    <div className="lg:px-52 min-h-screen mt-10">
+      <div>
+        <h1 className="text-xl font-bold pb-5">Codex</h1>
+      </div>
+      <Divider />
+      <div className="grid grid-cols-1 lg:grid-cols-3 lg:min-h-[78vh]">
+        <section className="col-span-1 lg:border-r lg:pr-5 py-5 h-full">
+          {menu.map((item) => (
+            <div
+              onClick={() => handleClick(item)}
+              key={item.name}
+              className={`${item.path === location.pathname?"bg-primary-color text-white":""} py-3 cursor-pointer hover:text-white hover:bg-primary-color
+                        px-5 rounded-md border-b`}
+            >
+              <a href={item.path}>{item.name}</a>
+            </div>
+          ))}
+        </section>
+        <section className="right lg:col-span-2 lg:pl-5 py-5">
+            <Orders/>
+        </section>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
--- src/customer/pages/Account/OrderItem.jsx
+++ src/customer/pages/Account/OrderItem.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import { Avatar } from "@mui/material";
+import { ElectricBolt } from "@mui/icons-material";
+import React from "react";
 
 const OrderItem = () => {
   return (
-    <div>
-      
+    <div className="text-sm bg-white p-5 space-y-4 border rounded-md cursor-pointer">
+      <div className="flex items-center gap-5">
+        <div>
+          <Avatar sizes="small" sx={{ bgcolor: "#003366" }}>
+            <ElectricBolt />
+          </Avatar>
+        </div>
+        <div>
+          <h1 className="font-bold text-primary-color">PENDING</h1>
+          <p>Arriving By Mon, 12 jan</p>
+        </div>
+      </div>
+      <div className="p-5 bg-blue-50 flex gap-3 ">
+        <div>
+          <img className="w-[70px]"
+            src="https://res.cloudinary.com/dxoqwusir/image/upload/v1727452042/pro-ray-android-ios-cellecor-yes-original-imagydnsrany7qhy_1_m9n9t5.webp"
+            alt=""
+          />
+        </div>
+        <div className="w-full space-y-2 ">
+            <h1 className="font-bold">Kp Clothing</h1>
+            <p>Cellecor RAY 1.43" AMOLED Display | 700 NITS | AOD | BT-Calling | AI Voice | Split Screen Smartwatch (Black Strap, Free Size)</p>
+            <p>
+                <strong>size : </strong>
+                FREE
+            </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OrderItem
+export default OrderItem;
